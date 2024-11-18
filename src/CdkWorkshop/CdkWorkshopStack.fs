f9c14aa99ec4d7d37c360bcfd3272ec27053582d
Kurt Mueller <kurt.mueller@osumc.edu>
2024-11-18 11:12:59 -0500

    Add lambda function

--- src/CdkWorkshop/CdkWorkshopStack.fs
+++ src/CdkWorkshop/CdkWorkshopStack.fs
@@ -1,6 +1,7 @@
 namespace CdkWorkshop
 
 open Amazon.CDK
+open Amazon.CDK.AWS.Lambda
 open Amazon.CDK.AWS.SNS
 open Amazon.CDK.AWS.SNS.Subscriptions
 open Amazon.CDK.AWS.SQS
@@ -9,14 +10,14 @@ open Constructs
 type CdkWorkshopStack(scope: Construct, id: string, props: IStackProps) as this =
     inherit Stack(scope, id, props)
 
-    let queue = Queue(this, "CdkWorkshopQueue", QueueProps(VisibilityTimeout = Duration.Seconds(300.)))
-
-    let topic = Topic(this, "CdkWorkshopTopic")
-
-    let subscription = SqsSubscription(queue)
+    let hello = Function(this, "HelloHandler", FunctionProps(
+        Runtime = Runtime.NODEJS_LATEST,
+        Code = Code.FromAsset("lambda"),
+        Handler = "hello.handler"
+    ))
 
     do
         // subscribe the queue to receive any messages published to the topic
-        topic.AddSubscription(subscription) |> ignore
+        // topic.AddSubscription(subscription) |> ignore
 
         ()
