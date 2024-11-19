8a25638b6f6c1d336a3e59f4cde9070e6c3b4308
Kurt Mueller <kurt.mueller@osumc.edu>
2024-11-19 10:31:48 -0500

    Add CodePipeline setup to WorkshopPipelineStack for CI/CD integration

--- src/CdkWorkshop/WorkshopPipelineStack.fs
+++ src/CdkWorkshop/WorkshopPipelineStack.fs
@@ -1,9 +1,27 @@
 namespace CdkWorkshop
 
 open Amazon.CDK
-open Amazon.CDK.AWS.CodeCommit
+open Amazon.CDK.Pipelines
 open Constructs
 
 type WorkshopPipelineStack(scope: Construct, id: string, props: StackProps) as this =
     inherit Stack(scope, id, props)
 
+    let connSourceOptions = ConnectionSourceOptions(
+        ConnectionArn = "arn:aws:codeconnections:us-east-1:452283499101:connection/95189adc-e57b-4ccf-ac4e-004e473735d2"
+    )
+    let source = CodePipelineSource.Connection(repoString = "kurt-mueller-osumc/cdkworkshop", branch = "main", props = connSourceOptions)
+
+    let pipeline = CodePipeline(this, "Pipeline", CodePipelineProps(
+        PipelineName = "WorkshopPipeline",
+        Synth = CodeBuildStep( "SynthStep",
+            CodeBuildStepProps(
+                Input = source,
+                // Commands = [| "npm ci"; "npm run build"; "npx cdk synth" |]
+                Commands = [| "npx cdk synth" |]
+            )
+        )
+    ))
+
+    do
+        ()
